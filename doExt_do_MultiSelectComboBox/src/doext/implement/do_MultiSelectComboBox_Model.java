0e785fca2c81b4950cb92af97236643c6a24a460
maorui <maoruiily@163.com>
2015-07-23 15:53:30 +0800

    init

---
+++ doExt_do_MultiSelectComboBox/src/doext/implement/do_MultiSelectComboBox_Model.java
@@ -0,0 +1,15 @@
+package doext.implement;
+
+import doext.define.do_MultiSelectComboBox_MAbstract;
+
+/**
+ * 自定义扩展组件Model实现，继承do_MultiSelectComboBox_MAbstract抽象类；
+ *
+ */
+public class do_MultiSelectComboBox_Model extends do_MultiSelectComboBox_MAbstract {
+
+	public do_MultiSelectComboBox_Model() throws Exception {
+		super();
+	}
+	
+}
